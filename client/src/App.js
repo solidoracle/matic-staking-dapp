c528858059d44645b3f7652ed6a5904ce78be86a
solidoracle <solidoracle17@gmail.com>
2022-07-07 18:14:10 +0200

    update frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import react, { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import artifacts from './artifacts/contracts/Staking.sol/Staking.json' //that gets created when we deoploy our contract (we specified this location in our config file)
 
+import NavBar from './components/NavBar.jsx'
 
 const CONTRACT_ADDRESS ='0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9'
 
@@ -129,7 +130,15 @@ function App() {
 // UI incorporate
   return (
     <div className="App">
-      
+      <div>
+        <NavBar // component we are definin in 
+        isConnected={isConnected}
+        connect={connectAndLoad}
+
+
+
+
+      </div>
 
 
 
