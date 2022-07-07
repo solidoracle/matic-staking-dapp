213012073ecc3f90fc3535e71b9ec4bdefe598a6
solidoracle <solidoracle17@gmail.com>
2022-07-07 12:32:03 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import react, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import artifacts from './artifacts/contracts/Staking.sol/Staking.json' //that gets created when we deoploy our contract
+
+
 
 function App() {
   return (
