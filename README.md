1009a0535914b89383ff7e289cdde1892c24053f
solidoracle <solidoracle17@gmail.com>
2022-07-08 11:59:52 +0200

    front end update

---
+++ README.md
@@ -0,0 +1,18 @@
+Packages to install
+
+
+
+DEPLOY
+npx hardhat run --network localhost scripts/1_deploy.js
+
+
+
+FRONTEND
+// client is the directory where we put all our front end code
+npx create-react-app client
+(if there is a conflict with src if means you have already deployed the contract and created the directory, so delete client folder and run the command again without first deploying)
+cd client
+npm i react-bootstrap-icons ethers bootstrap
+
+npm start to start the app
+
--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 import artifacts from './artifacts/contracts/Staking.sol/Staking.json' //that gets created when we deoploy our contract (we specified this location in our config file)
 
 import NavBar from './components/NavBar.jsx'
+import { Bank, PiggyBank, Coin } from 'react-bootstrap-icons'
 
 const CONTRACT_ADDRESS ='0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9'
 
@@ -154,15 +155,106 @@ return (
                 <span className='glyph'>
                   <Coin /> // comes from react bootstrap icons
                 </span>
+              </div>
+              <div className='optionData'>
+                <span>1 Month</span>
+                <span className='optionPercent'>7%</span>
+              </div>
+            </div>
+          </div>
 
+          <div className='col-md-4'>
+            <div onClick={() => openStakingModal(90, '10%')} className='marketOption'>
+              <div className='glyphContainer hoverButton'>
+                <span className='glyph'>
+                  <Coin /> // comes from react bootstrap icons
+                </span>
+              </div>
+              <div className='optionData'>
+                <span>3 Months</span>
+                <span className='optionPercent'>10%</span>
+              </div>
+            </div>
+          </div>
 
-
+          <div className='col-md-4'>
+            <div onClick={() => openStakingModal(180, '12%')} className='marketOption'>
+              <div className='glyphContainer hoverButton'>
+                <span className='glyph'>
+                  <Coin /> // comes from react bootstrap icons
+                </span>
+              </div>
+              <div className='optionData'>
+                <span>6 Months</span>
+                <span className='optionPercent'>12%</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>      
+    </div>     
+
+    <div className='assetContainer'>
+      <div className='subContainer'>
+        <span className='marketHeader'>Staked Assets</span>
+      </div>
+      <div>
+        <div className='row columnHeaders'>
+            <div className='col-md-2'>Assets</div>
+            <div className='col-md-2'>Percent Interest</div>
+            <div className='col-md-2'>Staked</div>
+            <div className='col-md-2'>Interest</div>
+            <div className='col-md-2'>Days remaining</div>
+            <div className='col-md-2'></div>
+        </div>
+      </div>
+      <br />
+      {assets.length > 0 && assets.map((a, idx) => (
+        <div className='row'>
+          <div className='col-md-2'>
+            <span>
+              <img className='stakedLogoImg' src='eth-logo.webp' />
+            </span>
+          </div>
+          <div className='col-md-2'>
+            {a.percentInterest} %
+          </div>
+          <div className='col-md-2'>
+            {a.plegStaked} 
+          </div>
+          <div className='col-md-2'>
+            {a.plegInterest} 
+          </div>
+          <div className='col-md-2'>
+            {a.daysRemainig} 
+          </div>
+          <div className='col-md-2'>
+            {a.open ? (
+              <div onClick={() => withdraw(a.positionId)} className='orangeMiniButton'>Withdraw</div>
+            ) : (
+              <span>closed</span>
+            )} %
+          </div>
+        </div>
+      ))}
+    </div>
+  <div>
+  </div>
+  {showStakeModal && (
+    <StakeModal
+    onClose={() => setShowStakeModal(false)}
+    stakingLength={stakingLength}
+    stakingLPercent={stakingLPercent}
+    amount={amont}
+    setAmount={setAmount}
+    stakePleg={stakePleg}
+    />
+  )}
+
+
+
+
+
   </div>
 );
 }
