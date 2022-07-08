e5c5db88e4fc57e6f456d674322216b518a1e1c0
solidoracle <solidoracle17@gmail.com>
2022-07-08 12:11:21 +0200

    stake modal

---
+++ client/src/components/StakeModal.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const StakeModal = props => {
+    const {
+        onClose,
+        stakingLength,
+        stakingPercent,
+        setAmount,
+        stakePleg,
+    } = props
+
+    return {
+        <>
+            <div className='modal-class' onClick={{props.onClose}}>
+                <div className='modal-content' onClick={e => e.stopPropagation()}>
+                    <div className='modal-body'>
+                        <h2 className='titleHeader'>Stake Pleg</h2>
+
+                        <div className='row'>
+                            <div className='col-md-9 fieldContainer'>
+                                <input
+                                className='inputField'
+                                placeholder='0.0'
+                                onChange={e => props.setAmount(e.target.value)}
+                                />
+                            </div>
+                            <div className='col-md-3 inputFieldUnitsContainer'>
+                                <span>PLEG</span>
+                            </div>
+                        </div>
+
+                        <div className='row'>
+                            <h6 className='titleHeader stakingTerms'>{stakingLength} days @ {stakingPercent} APY</APY></h6>
+                        </div>
+                        <div className='row'>
+                           <div
+                            onClick={() => stakePleg()}   
+                            className='orangeButton'                         
+                           >
+                               Stake
+                        </div>
+
+
+                    </div>
+                </div>        
+            </div>
+        </>
+    }
+
+
+
+}
