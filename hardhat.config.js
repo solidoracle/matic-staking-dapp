3365c98ab6b092bd4407ed8e32efb1180123f719
solidoracle <solidoracle17@gmail.com>
2022-07-10 17:50:46 +0200

    networks and .env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -7,7 +8,16 @@ module.exports = {
   solidity: {
     version: "0.8.0",
   },
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    matic: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
   paths: {
     artifacts: "./client/src/artifacts", // we are changing the location where the artifacts are generated when we compile so the frontend has easier access to them
-  }
+  }  
 };
